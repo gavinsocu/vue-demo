55b6d90ebf0e4a03fad150aca59fd3a13a9a2ba5
gavinsocu <125785620+gavinsocu@users.noreply.github.com>
2024-02-08 13:39:16 +0800

    更换*通配符为'/:catchAll(.*)'，适应vue-router的更新

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/:catchAll(.*)', redirect: '/' },
     {
       path: '/',
       name: '',
       redirect: '/home',
     },
-    { path: '*', redirect: '/' },
+
     {
       path: '/home',
       name: 'home',
